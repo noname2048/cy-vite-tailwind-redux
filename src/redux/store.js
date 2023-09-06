4c33bda3e91b7b7bbb77a63c61007a73a1b8dfc8
Sungwook Choi <sungwook.csw@gmail.com>
2023-09-06 22:54:30 +0900

    feat: Apply devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,19 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
 import counterReducer from "#/redux/counterReducer.js";
 import todosReducer from "#/redux/todosReducer.js";
 import logger from "redux-logger";
+import { composeWithDevTools } from "@redux-devtools/extension";
+import ReduxThunk from "redux-thunk";
 
 const rootReducer = combineReducers({
   counter: counterReducer,
   todos: todosReducer,
 });
 
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(ReduxThunk, logger)),
+);
 
 export default store;
 export { store };

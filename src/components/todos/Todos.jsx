49e9deb9b5ec28c467405a6fe75fed354e3fe288
Sungwook Choi <sungwook.csw@gmail.com>
2023-09-06 22:33:05 +0900

    feat: Implement todos

---
+++ src/components/todos/Todos.jsx
@@ -0,0 +1,127 @@
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addTask,
+  deleteTask,
+  updateTask,
+  clearTask,
+} from "#/redux/todosReducer.js";
+import PropTypes from "prop-types";
+import { useState } from "react";
+
+export default function TodosContainer() {
+  const { tasks } = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
+  return <Todos tasks={tasks} dispatch={dispatch} />;
+}
+
+function Todos({ tasks, dispatch }) {
+  const [text, setText] = useState("");
+  return (
+    <div>
+      <div>
+        <input
+          className="border-2 m-2 p-1 rounded-md"
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button
+          className="border p-1 m-2"
+          onClick={() => {
+            dispatch(addTask(text));
+            setText("");
+          }}
+        >
+          ADD
+        </button>
+        <button
+          className="border p-1 m-2"
+          onClick={() => {
+            dispatch(clearTask());
+            setText("");
+          }}
+        >
+          CLEAR
+        </button>
+      </div>
+      {tasks.map((task) => (
+        <Task key={task.id} task={task} dispatch={dispatch} />
+      ))}
+    </div>
+  );
+}
+
+Todos.propTypes = {
+  tasks: PropTypes.array.isRequired,
+  dispatch: PropTypes.func.isRequired,
+};
+
+function Task({ task, dispatch }) {
+  const [isEdit, setIsEdit] = useState(false);
+
+  let content;
+  if (isEdit) {
+    content = (
+      <>
+        <input
+          className="px-1 mx-1 border-2"
+          type="text"
+          value={task.text}
+          onChange={(e) => {
+            console.log(e.target.value);
+            dispatch(updateTask({ ...task, text: e.target.value }));
+          }}
+        />
+        <button
+          className="border p-1"
+          onClick={() => {
+            setIsEdit(false);
+          }}
+        >
+          Save
+        </button>
+      </>
+    );
+  } else {
+    content = (
+      <>
+        <span className="px-1 mx-1">{task.text}</span>
+        <button
+          className="border p-1 rounded-md mx-1"
+          onClick={() => {
+            setIsEdit(true);
+          }}
+        >
+          Edit
+        </button>
+      </>
+    );
+  }
+
+  return (
+    <div className="m-2">
+      <input
+        className="px-1 mx-1"
+        type="checkbox"
+        value={task.isCompleted}
+        onChange={(e) => {
+          dispatch(updateTask({ ...task, isCompleted: e.target.value }));
+        }}
+      />
+      {content}
+      <button
+        className="border p-1 rounded-md mx-1"
+        onClick={() => {
+          dispatch(deleteTask(task.id));
+        }}
+      >
+        Delete
+      </button>
+    </div>
+  );
+}
+
+Task.propTypes = {
+  task: PropTypes.object.isRequired,
+  dispatch: PropTypes.func.isRequired,
+};
--- src/redux/todosReducer.js
+++ src/redux/todosReducer.js
@@ -1,34 +1,34 @@
 export const ADD_TASK = "task/ADD_TASK";
 export const DELETE_TASK = "task/DELETE_TASK";
 export const UPDATE_TASK = "task/UPDATE_TASK";
-export const EDIT_TASK = "task/EDIT_TASK";
 export const CLEAR_TASK = "task/CLEAR_TASK";
 
-export const addTask = (task) => ({ type: ADD_TASK, payload: task });
+export const addTask = (text) => ({
+  type: ADD_TASK,
+  payload: { id: taskId++, text, isCompleted: false },
+});
 export const deleteTask = (id) => ({ type: DELETE_TASK, payload: id });
 export const updateTask = (task) => ({ type: UPDATE_TASK, payload: task });
-export const editTask = (task) => ({ type: EDIT_TASK, payload: task });
 export const clearTask = () => ({ type: CLEAR_TASK });
 
 const initialState = {
   tasks: [],
 };
 
+let taskId = 1000;
+
 const todosReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_TASK:
       return {
-        ...state,
         tasks: [...state.tasks, action.payload],
       };
     case DELETE_TASK:
       return {
-        ...state,
         tasks: state.tasks.filter((task) => task.id !== action.payload),
       };
     case UPDATE_TASK:
       return {
-        ...state,
         tasks: state.tasks.map((task) => {
           if (task.id === action.payload.id) {
             return { ...action.payload };
@@ -36,19 +36,8 @@ const todosReducer = (state = initialState, action) => {
           return task;
         }),
       };
-    case EDIT_TASK:
-      return {
-        ...state,
-        tasks: state.tasks.map((task) => {
-          if (task.id === action.payload.id) {
-            return { ...task, title: action.payload.title };
-          }
-          return task;
-        }),
-      };
     case CLEAR_TASK:
       return {
-        ...state,
         tasks: [],
       };
     default:

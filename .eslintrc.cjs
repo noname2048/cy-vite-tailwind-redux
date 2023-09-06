f8e6a1b90f9f803d7a42522e2124ef815735c917
Sungwook Choi <sungwook.csw@gmail.com>
2023-09-06 19:32:03 +0900

    feat: Enable prettier config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,29 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    'plugin:react-hooks/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    requireConfigFile: false,
+    allowImportExportEverywhere: true,
+    babelOptions: {
+      presets: ['next/babel'],
+      caller: { supportsTopLevelAwait: true }
+    }
+  },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
-      { allowConstantExport: true },
-    ],
-  },
+      { allowConstantExport: true }
+    ]
+  }
 }
